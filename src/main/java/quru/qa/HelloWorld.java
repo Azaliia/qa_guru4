8f7ddb88598b47d4fdd913b8461c6ee715b8c053
azaliazakirova <azalia-90@ya.ru>
2022-09-29 16:52:36 +0300

    HomeWork_4

---
+++ src/main/java/quru/qa/HelloWorld.java
@@ -0,0 +1,19 @@
+package quru.qa;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int a=125, b=125, c=0;
+        double d=1.5;
+
+        //String stringThatShouldDisplayInConsole = "Hello, World";
+        //c=a+d;
+
+        if (a>d)
+        {
+            System.out.println("a > d");
+        }
+
+        //System.out.println("Rezult "+ c);
+    }
+}
